40b404545bc608c9d6ff92d2319f210faa9d4522
denislav-d <ddenislav333@gmail.com>
2023-06-01 15:58:34 +0200

    Animation changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="mt-6 md:w-[44rem] lg:w-[54rem] py-12 px-6 md:py-16 md:px-8 lg:py-24 lg:px-16">
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 230,
+        damping: 20,
+      }}
+      className="mt-6 md:w-[44rem] lg:w-[54rem] py-12 px-6 md:py-16 md:px-8 lg:py-24 lg:px-20"
+    >
       <h1 className="text-[2.5rem] md:text-6xl lg:text-7xl font-bold tracking-tighter leading-10">
         welcome{" "}
         <span className="inline-block animate-wave origin-[70%_70%]">👋</span>,
@@ -10,12 +20,22 @@ export default function Home() {
         <br />
         design driven developer & fashion enthusiast.
       </h1>
-      <p className="mt-8 text-md lg:text-lg text-[#676362] lg:w-[42rem] leading-6">
+      <motion.p
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 230,
+          damping: 20,
+          delay: 0.4,
+        }}
+        className="mt-8 text-md lg:text-lg text-[#676362] lg:w-[42rem] leading-6"
+      >
         I am a student, who is currently pursuing a university degree in the
         Netherlands. Having a long-time ambition for all types of design and my
         rapidly growing interest and knowledge in the web development sector are
         pushing me towards improving in my desired field of work.
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 }
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -4,35 +4,35 @@ import { motion } from "framer-motion";
 const tabs = [
   {
     id: "home",
-    label: "Home",
+    label: "home",
   },
   {
     id: "projects",
-    label: "Projects",
+    label: "projects",
   },
   {
     id: "about",
-    label: "About",
+    label: "about",
   },
 ];
 
 export default function NavMenu() {
   const [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
-    <div className="flex space-x-1">
+    <div className="flex space-x-1 bg-[rgba(250, 250, 250, 0.94)]  rounded-full backdrop-blur-1">
       {tabs.map((tab) => (
         <button
           key={tab.id}
           onClick={() => setActiveTab(tab.id)}
           className={`${
-            activeTab === tab.id ? "" : "hover:text-white/60"
-          } relative rounded-full px-3 py-1.5 text-lg lg:text-2xl font-medium text-zinc-500 outline-sky-400 transition focus-visible:outline-2`}
+            activeTab === tab.id ? "" : "hover:text-[#d8d5d0]"
+          } relative rounded-full px-3 py-1.5 text-lg lg:text-md font-medium text-[#aba8a4] outline-sky-400 transition focus-visible:outline-2`}
           style={{ WebkitTapHighlightColor: "transparent" }}
         >
           {activeTab === tab.id && (
             <motion.span
               layoutId="bubble"
-              className="absolute inset-0 z-10 bg-white mix-blend-difference"
+              className="absolute inset-0 z-10 bg-black mix-blend-difference"
               style={{ borderRadius: 9999 }}
               transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
             />
