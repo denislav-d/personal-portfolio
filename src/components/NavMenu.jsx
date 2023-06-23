ac10eab278ecabf55d3dd26bf39c860dfb494d5f
denislav-d <ddenislav333@gmail.com>
2023-06-24 01:07:06 +0200

    Gradient blob added

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -11,28 +11,28 @@ const tabs = [
     label: "projects",
   },
   {
-    id: "about",
-    label: "about",
+    id: "contact",
+    label: "contact",
   },
 ];
 
 export default function NavMenu() {
   const [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
-    <div className="flex space-x-1 bg-[rgba(250, 250, 250, 0.94)]  rounded-full backdrop-blur-1">
+    <div className="flex space-x-1 bg-[rgb(250, 250, 250)]  rounded-full backdrop-blur-1 shadow-lg p-1">
       {tabs.map((tab) => (
         <button
           key={tab.id}
           onClick={() => setActiveTab(tab.id)}
           className={`${
-            activeTab === tab.id ? "" : "hover:text-[#d8d5d0]"
-          } relative rounded-full px-3 py-1.5 text-lg lg:text-md font-medium text-[#aba8a4] outline-sky-400 transition focus-visible:outline-2`}
+            activeTab === tab.id ? "" : "hover:text-[#676362]"
+          } relative rounded-full px-3 py-1.5 text-lg lg:text-md font-medium text-[#CBC3E3] outline-sky-400 transition focus-visible:outline-2`}
           style={{ WebkitTapHighlightColor: "transparent" }}
         >
           {activeTab === tab.id && (
             <motion.span
               layoutId="bubble"
-              className="absolute inset-0 z-10 bg-black mix-blend-difference"
+              className="absolute inset-0 z-10 bg-[#CBC3E3] mix-blend-difference"
               style={{ borderRadius: 9999 }}
               transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
             />
