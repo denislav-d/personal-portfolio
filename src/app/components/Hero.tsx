e4b3bb504fc65fc3038fc70eaa3e38eb7eec8b6d
denislav-d <ddenislav333@gmail.com>
2024-01-30 12:08:10 +0200

    Hero section and project component improvements

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,17 +1,7 @@
-import { motion } from "framer-motion";
-
 export default function Home() {
   return (
     <section className="container mx-auto px-6 md:px-24 lg:px-16 grid lg:grid-cols-3 grid-cols-1 max-w-screen-xl">
-      {/* <section className="container mx-auto px-6 md:px-24 lg:px-16 flex justify-center"> */}
-      <motion.div
-        initial={{ opacity: 0, y: -35 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          duration: 0.85,
-        }}
-        className="col-span-2 mt-16 lg:mt-36"
-      >
+      <div className="col-span-2 mt-16 lg:mt-36">
         <h1 className="text-[2.4rem] sm:text-5xl md:text-[3.5rem] lg:text-6xl font-bold tracking-tighter leading-10 text-dark">
           welcome
           <span className="ml-1 relative inline-block animate-wave origin-[70%_70%]">
@@ -23,21 +13,13 @@ export default function Home() {
           design driven developer
           <br /> & fashion enthusiast.
         </h1>
-        <motion.p
-          initial={{ opacity: 0, y: -35 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.85,
-            delay: 0.25,
-          }}
-          className="mt-8 text-md lg:text-lg text-gray leading-6 max-w-screen-sm"
-        >
+        <p className="mt-8 text-md lg:text-lg text-gray leading-6 max-w-screen-sm">
           I am a student, who is currently pursuing a university degree in the
           Netherlands. Having a long-time ambition for all types of design and
           my rapidly growing interest and knowledge in the web development
           sector are pushing me towards improving in my desired field of work.
-        </motion.p>
-      </motion.div>
+        </p>
+      </div>
     </section>
   );
 }
--- src/app/components/ProjectList.tsx
+++ src/app/components/ProjectList.tsx
@@ -1,21 +1,8 @@
 import { motion } from "framer-motion";
 import { Drawer } from "vaul";
-import Image from "next/image";
+import Project from "@/app/components/Project";
 
 import { projects } from "@/app/data";
-
-interface Project {
-  id: number;
-  title: string;
-  date: string;
-  type: string;
-  description: string;
-  image?: any;
-}
-
-interface ProjectProps {
-  article: Project;
-}
 
 export default function ProjectList() {
   return (
@@ -71,37 +58,5 @@ export default function ProjectList() {
         ))}
       </div>
     </section>
-  );
-}
-
-function Project({ article }: ProjectProps) {
-  return (
-    <>
-      <div className="rounded-2xl shadow-lg relative aspect-[15/8] flex-shrink-0 hover:scale-[0.99] shadow-stone-600/[0.03] hover:shadow-md hover:shadow-stone-600/5 bg-gray-200 duration-300">
-        <div className="flex h-full w-full items-center justify-center">
-          <Image
-            className="h-full w-full overflow-hidden object-contain rounded-2xl"
-            src={article.image}
-            alt={`${article.title} image`}
-            width="1920"
-            height="1024"
-          />
-        </div>
-      </div>
-      <div className="px-1.5">
-        <div className="mt-3 flex justify-between">
-          <div className="font-medium text stone-500">{article.date}</div>
-          <span>{article.type}</span>
-        </div>
-        <h3 className="mt-3 text-2xl font-bold tracking-tight">
-          <a className="outline-none">{article.title}</a>
-        </h3>
-        <div className="mt-2 text-stone-600">
-          <a className="outline-none">
-            <div>{article.description}</div>
-          </a>
-        </div>
-      </div>
-    </>
   );
 }
