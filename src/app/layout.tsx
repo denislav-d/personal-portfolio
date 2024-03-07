f27c854b49351cac850bf9335544b67cea66db85
denislav-d <ddenislav333@gmail.com>
2024-03-07 10:13:43 +0100

    Added emoji favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Denislav Dimitrov",
   description: "Denislav's personal portfolio",
+  icons:
+    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>◻️</text></svg>",
 };
 
 export default function RootLayout({
