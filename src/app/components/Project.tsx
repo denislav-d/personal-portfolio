8903d5b713f6a44321ba596b33c84fb4b14ba229
denislav-d <ddenislav333@gmail.com>
2024-03-12 12:10:50 +0100

    Lighthouse score optimisation

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -18,17 +18,13 @@ export default function Project({ article }: ProjectProps) {
   return (
     <div className="cursor-pointer hover:scale-[0.97] duration-300">
       <div className="flex justify-between items-center text-light">
-        <h3 className="text-2xl font-semibold tracking-tighter">
-          <a className="outline-none before:inline-block before:w-[.85rem] before:h-[.85rem] before:bg-white before:mr-2 before:content-['']">
-            {article.title}
-          </a>
-        </h3>
-        <h5 className="font-medium">{article.type}</h5>
+        <h1 className="text-2xl font-semibold tracking-tighter outline-none before:inline-block before:w-[.85rem] before:h-[.85rem] before:bg-white before:mr-2 before:content-['']">
+          {article.title}
+        </h1>
+        <h2 className="font-medium">{article.type}</h2>
       </div>
-      <div className="mt-2 text-light text-[1.1rem]">
-        <a className="outline-none">
-          <div>{article.description}</div>
-        </a>
+      <div className="mt-2 text-light text-[1.1rem] outline-none">
+        <div>{article.description}</div>
       </div>
     </div>
   );
