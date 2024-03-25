1c78f8e77aba918630e1462fb294f202950293e2
denislav-d <ddenislav333@gmail.com>
2024-03-25 21:24:21 +0100

    Fixed staggering animation

--- src/app/components/ProjectDescription.tsx
+++ src/app/components/ProjectDescription.tsx
@@ -19,39 +19,78 @@ export default function ProjectDescription({
     link_client,
   },
 }: ProjectProps) {
-  return (
-    <motion.article
-      viewport={{ once: true }}
-      whileInView={{ opacity: 1, y: 0 }}
-      initial={{ opacity: 0, y: 50 }}
-      // TODO figure out staggered children animation
-      transition={{
+  const frameVariants = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+
+      transition: {
+        staggerChildren: 0.15,
         duration: 0.3,
         type: "spring",
         damping: 14,
-      }}
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
+  return (
+    <motion.article
+      variants={frameVariants}
+      initial="hidden"
+      animate="visible"
       className="container mx-auto mb-36 mt-24 grid grid-cols-1 gap-8 px-12 md:w-full md:min-w-[512px] md:max-w-xl lg:mt-36"
     >
-      <h1 className="font-serif text-lg font-medium tracking-tight text-dark before:mr-2 before:inline-block before:h-[0.6rem] before:w-[0.6rem] before:bg-dark before:content-['']">
+      <motion.h1
+        className="font-serif text-lg font-medium tracking-tight text-dark before:mr-2 before:inline-block before:h-[0.6rem] before:w-[0.6rem] before:bg-dark before:content-['']"
+        variants={itemVariants}
+      >
         {title}
         <br />
         <span className="font-sans text-base font-normal italic tracking-normal text-gray">
           {subheading}
         </span>
-      </h1>
+      </motion.h1>
       {link_before ? (
-        <DescriptionBlock
-          title="context"
-          link={link_client}
-          link_before={link_before}
-          description={context_description}
-        />
+        <motion.div variants={itemVariants}>
+          <DescriptionBlock
+            title="context"
+            link={link_client}
+            link_before={link_before}
+            description={context_description}
+          />
+        </motion.div>
       ) : (
-        <DescriptionBlock title="context" description={context_description} />
+        <motion.div>
+          <DescriptionBlock title="context" description={context_description} />
+        </motion.div>
       )}
 
-      <DescriptionBlock title="my role" description={myrole_description} />
-      <BackButton />
+      <motion.div variants={itemVariants}>
+        <DescriptionBlock title="my role" description={myrole_description} />
+      </motion.div>
+
+      <motion.div variants={itemVariants}>
+        <BackButton />
+      </motion.div>
     </motion.article>
   );
 }
