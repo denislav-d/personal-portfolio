d6b7472be51500f2e1b8ffcbb274de48dcbb2cbf
denislav-d <ddenislav333@gmail.com>
2024-04-07 01:51:08 +0200

    About page progress
    Still need to incorporate a new hero page, including about page and projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,7 +8,7 @@ export default function Project({ project }: ProjectProps) {
   return (
     <>
       <div className="mb-2 flex items-center justify-between text-dark">
-        <h1 className="text-title">{project.title}</h1>
+        <h1 className="text-title underline">{project.title}</h1>
       </div>
       <div className="text-secondary">{project.description}</div>
     </>
