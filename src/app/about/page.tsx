b009309948226acdc16c3a9a53f10594d93d6079
denislav-d <ddenislav333@gmail.com>
2024-05-01 18:33:42 +0300

    Quick change in about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
     <section className="container z-10 mx-auto mb-20 mt-24 flex flex-col px-10 md:w-full md:min-w-[512px] md:max-w-xl lg:mt-36">
       <h1 className="text-title">about 👨🏻‍💻</h1>
       <p className="text-secondary mb-4">
-        this is my personal portfolio website where I showcase my projects and
+        my personal portfolio website where I showcase my projects and
         cotributions by creatively displaying them in my own minimal
         environment.
       </p>
