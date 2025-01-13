804277f4cb23652afa547be404c650c5641cdb0c
denislav-d <ddenislav333@gmail.com>
2025-01-13 15:33:50 +0100

    Add new website WIP section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ export default function LandingPage() {
   return (
     <main className="mt-24 lg:mt-36">
       <Hero />
+      <section className="container z-10 mx-auto mb-20 flex flex-col px-10 md:w-full md:min-w-[512px] md:max-w-xl">
+        <h2 className="text-title">New website design coming soon 🔧 ...</h2>
+      </section>
       <ProjectList />
     </main>
   );
